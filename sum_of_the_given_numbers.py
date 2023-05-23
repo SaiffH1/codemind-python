08b70834133c711bbcd44dc0df2149299ed52861
SaiffH1 <98868333+SaiffH1@users.noreply.github.com>
2023-05-23 10:36:11 +0530

    N = int(input())  # Read the value of N from the user
    for _ in range(N):
        a, b = map(int, input().split())  # Read two integers from each line and assign them to variables a and b
        sum = a + b  # Calculate the sum of the two numbers
        print(sum)  # Print the sum on a separate line

--- sum_of_the_given_numbers.py
+++ sum_of_the_given_numbers.py
@@ -1,6 +1,6 @@
-n = int(input())
-for i in range(1,n+1):
-    flag = 0
-    x,y=map(int,input().split())
-    sum=x+y
-    print(sum)
+N = int(input())  # Read the value of N from the user
+
+for _ in range(N):
+    a, b = map(int, input().split())  # Read two integers from each line and assign them to variables a and b
+    sum = a + b  # Calculate the sum of the two numbers
+    print(sum)  # Print the sum on a separate line
