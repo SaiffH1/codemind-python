7e61067bae62a10f18e62f64b3bdf0acf73a09a7
SaiffH1 <98868333+SaiffH1@users.noreply.github.com>
2023-05-26 14:26:43 +0530

    n=int(input()) s=list(map(int,input().split())) count=0 for i in range (1,len(s)-1):     if s[i-1]%2==0 and s[i+1]%2==0:         if s[i]%2!=0:             count+=1 print(count)

---
+++ count_odd_numbers_between_even_numbers.py
@@ -0,0 +1,8 @@
+n=int(input())
+s=list(map(int,input().split()))
+count=0
+for i in range (1,len(s)-1):
+    if s[i-1]%2==0 and s[i+1]%2==0:
+        if s[i]%2!=0:
+            count+=1
+print(count)
