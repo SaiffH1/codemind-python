cd34b73f7acc8f1fb88bb80cb2136f93e748d82b
SaiffH1 <98868333+SaiffH1@users.noreply.github.com>
2023-05-30 10:24:44 +0530

    import math def is_prime(z):     for i in range(2,int(math.sqrt(z))+1):         if z%i==0:             return False     return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if is_prime(i) and i!=1:         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,13 @@
+import math
+def is_prime(z):
+    for i in range(2,int(math.sqrt(z))+1):
+        if z%i==0:
+            return False
+    return True
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if is_prime(i) and i!=1:
+        c+=1
+print(c)
