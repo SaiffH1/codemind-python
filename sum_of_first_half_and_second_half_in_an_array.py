7d3bc5321ae77e63792fc6c2999c5fd0d82c2ced
SaiffH1 <98868333+SaiffH1@users.noreply.github.com>
2023-06-06 09:50:19 +0530

    n=int(input()) s=list(map(int,input().split())) a=[] b=[] for i in range(len(s)//2):     a.append(s[i]) for j in s:     if j not in a:         b.append(j) print(sum(a)) print(sum(b))

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,11 @@
+n=int(input())
+s=list(map(int,input().split()))
+a=[]
+b=[]
+for i in range(len(s)//2):
+    a.append(s[i])
+for j in s:
+    if j not in a:
+        b.append(j)
+print(sum(a))
+print(sum(b))
