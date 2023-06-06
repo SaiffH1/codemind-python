0e0eee0b6b07fc5ce344ccc7bc1ed43c2222918c
SaiffH1 <98868333+SaiffH1@users.noreply.github.com>
2023-06-06 14:06:29 +0530

    def vowels(i):     c=0     for j in i:         if j in "aeiou":             c+=1     return c n=input() s=n.split() a=[] for i in s:     x=vowels(i)     a.append(x) print(max(a))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,13 @@
+def vowels(i):
+    c=0
+    for j in i:
+        if j in "aeiou":
+            c+=1
+    return c
+n=input()
+s=n.split()
+a=[]
+for i in s:
+    x=vowels(i)
+    a.append(x)
+print(max(a))
