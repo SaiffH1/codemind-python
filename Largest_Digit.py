b0093ccb0d7fce50fea8b964be80b15ae90af3ee
SaiffH1 <98868333+SaiffH1@users.noreply.github.com>
2023-06-09 11:16:05 +0530

    n=input() x=max(list(n)) print(x)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,9 +1,3 @@
-number = int(input())
-count = 0 
-while number > 0:
-    digit = number % 10
-    if digit > count:
-        count = digit
-    number //= 10
-
-print(count)
+n=input()
+x=max(list(n))
+print(x)
